a3ae1cd768db22494b753cfeb526a018d8eb1844
Matt Ludwigs <mattludwigs@gmail.com>
2018-11-15 09:45:03 -0700

    Packege updates, license

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule HTS221.MixProject do
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      docs: [extras: ["README.md"], main: "readme"],
     ]
   end
 
@@ -21,11 +22,17 @@ defmodule HTS221.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-      {:circuits_i2c, path: "../circuits_i2c"},
       {:ex_doc, "~> 0.19", only: :dev, runtime: false},
-      {:dialyxir, "~> 0.5", only: [:dev], runtime: false}
+      {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
+      {:circuits_i2c, "~> 0.1.0"}
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Matt Ludwigs"],
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/mattludwigs/hts221"},
     ]
   end
 end
