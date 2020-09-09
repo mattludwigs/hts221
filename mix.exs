1cdf155b821ae4c39557234a83f43bb7be17b853
Matt Ludwigs <mattludwigs@users.noreply.github.com>
2020-09-09 11:04:24 -0700

    Deps, docs, and project configuration updates

--- mix.exs
+++ mix.exs
@@ -1,16 +1,22 @@
 defmodule HTS221.MixProject do
   use Mix.Project
 
+  @version "0.1.1"
+  @source_url "https://github.com/mattludwigs/hts221"
+
   def project do
     [
       app: :hts221,
-      version: "0.1.1",
+      version: @version,
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       docs: [extras: ["README.md"], main: "readme"],
       package: package(),
-      description: description()
+      dialyzer: dialyzer(),
+      docs: docs(),
+      description: description(),
+      preferred_cli_env: [docs: :docs, "hex.publish": :docs]
     ]
   end
 
@@ -24,9 +30,9 @@ defmodule HTS221.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, "~> 0.19", only: :dev, runtime: false},
-      {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
-      {:circuits_i2c, "~> 0.3.3"}
+      {:ex_doc, "~> 0.22", only: :docs, runtime: false},
+      {:dialyxir, "~> 1.0.0", only: [:dev], runtime: false},
+      {:circuits_i2c, "~> 0.3.6"}
     ]
   end
 
@@ -40,5 +46,21 @@ defmodule HTS221.MixProject do
 
   defp description() do
     "An Elixir library for working with the HTS221 sensor"
+  end
+
+  defp dialyzer() do
+    [
+      flags: [:race_conditions, :unmatched_returns, :error_handling, :underspecs]
+    ]
+  end
+
+  defp docs do
+    [
+      extras: ["README.md", "CHANGELOG.md"],
+      main: "readme",
+      source_ref: "v#{@version}",
+      source_url: @source_url,
+      skip_undefined_reference_warnings_on: ["CHANGELOG.md"]
+    ]
   end
 end
